45dd44a85246c1b40e474104a1a531e26ef5d62d
lyobzik <lyobzik@gmail.com>
2016-08-30 23:46:03 +0700

    Add simple consumer.

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "com.github.scopt" %% "scopt" % "3.5.0",
-  "com.typesafe" % "config" % "1.3.0"
+  "com.typesafe" % "config" % "1.3.0",
+  "org.apache.kafka" % "kafka_2.10" % "0.10.0.1"
 )
--- src/main/scala/main/scala.scala
+++ src/main/scala/main/scala.scala
@@ -1,5 +1,12 @@
 package main
 
+import java.util.Properties
+
+import kafka.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer}
+import org.apache.kafka.common.serialization._
+
+import collection.JavaConversions._
 
 case class Arguments(config: String = "/etc/kamish.conf")
 
@@ -8,7 +15,7 @@ class Config(configPath: String) {
   import com.typesafe._
   private[this] val conf = config.ConfigFactory.parseFile(new File(configPath))
 
-  // Let it crash
+  // Let it crash in incorrect config
   val inputKafkaServers = conf.getStringList("input.kafka.servers")
   val inputKafkaTopic = conf.getString("input.kafka.topic")
 
@@ -27,8 +34,39 @@ object HelloWorld extends App {
   parser.parse(args, Arguments()) match {
     case Some(arguments) =>
       val config = new Config(arguments.config)
+      val consumerConfig = Map(
+        "group.id" -> "kamish",
+        "bootstrap.servers" -> config.inputKafkaServers,
+        "key.deserializer" -> classOf[StringDeserializer].getName,
+        "value.deserializer" -> classOf[StringDeserializer].getName)
+      val consumer = new KafkaConsumer[String, String](consumerConfig)
+      consumer.subscribe(List(config.inputKafkaTopic))
+      println("subscription: " + consumer.subscription())
+      println("assignment: " + consumer.assignment())
+      consumer.seekToBeginning(consumer.assignment())
+
+      println("consumer: " + consumer)
       println("run with args: " + arguments.toString)
-      println("run with config: " + config.toString)
+      println("run with config: " + consumerConfig)
+
+      try {
+        while (true) {
+          val records = consumer.poll(1000)
+          println("subscription: " + consumer.subscription())
+          println("assignment: " + consumer.assignment())
+          if (records.nonEmpty) {
+            println("Read " + records.size + " records")
+          }
+          records.foreach((record: ConsumerRecord[String, String]) =>
+            println("Received record:\n  " + record.partition() + " - " +
+              record.offset() + ": " + record.value()))
+        }
+      } catch {
+        case err: Exception => println("Error: " + err)
+      } finally {
+        consumer.close()
+      }
+
     case None =>
   }
 }
